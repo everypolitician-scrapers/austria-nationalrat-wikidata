1ae0a9bb8d309e01b495cbaeaef54fbd40a1bd32
Tony Bowden <tony@tmtm.com>
2015-10-14 18:40:24 +0100

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,36 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'wikidata/fetcher'
+require 'nokogiri'
+require 'colorize'
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+
+def noko_for(url)
+  Nokogiri::HTML(open(URI.escape(URI.unescape(url))).read) 
+end
+
+def wikinames_from(url)
+  noko = noko_for(url)
+  names = noko.xpath('//h3[span[@id="Abgeordnete"]]/following-sibling::table[1]//tr[td]//td[1]//a[not(@class="new")]/@title').map(&:text)
+  abort "No names" if names.count.zero?
+  names
+end
+
+def fetch_info(names)
+  WikiData.ids_from_pages('de', names).each do |name, id|
+    data = WikiData::Fetcher.new(id: id).data('de') rescue nil
+    unless data
+      warn "No data for #{p}"
+      next
+    end
+    data[:original_wikiname] = name
+    ScraperWiki.save_sqlite([:id], data)
+  end
+end
+
+fetch_info wikinames_from('https://de.wikipedia.org/wiki/Liste_der_Abgeordneten_zum_%C3%96sterreichischen_Nationalrat_(XXV._Gesetzgebungsperiode)')
